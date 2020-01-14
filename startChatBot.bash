1fabee6023c2747ece735a0dc8f6e4941f38cbe6
RHEL Latam Technology Office <59034224+ltoRhelDemos@users.noreply.github.com>
2020-01-14 15:00:27 -0500

    Add files via upload

---
+++ startChatBot.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+/usr/bin/python3 serviceProvider.py &
+
+/usr/bin/python3 webChat.py botName=Ansible_Expert title=Ansible_Chatter subtitle=Latam_Technology_Office comment=maintained_by_Alejandro_Dirgan questionSentence=Your_question? buttonText=Ask&
+
+
