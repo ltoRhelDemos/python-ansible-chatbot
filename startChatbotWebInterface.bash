ac6262993b32ebcf3bed5b685b814202b4ad1372
RHEL Latam Technology Office <59034224+ltoRhelDemos@users.noreply.github.com>
2020-01-16 09:38:06 -0500

    Update startChatbotWebInterface.bash

--- startChatbotWebInterface.bash
+++ startChatbotWebInterface.bash
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
-python3 ./webChat.py botName=Ansible_Expert title=Ansible_Chabot subtitle=Red_Hat_Demo comment=Latam_Technology_Office questionSentence=Yout_Question? buttonT
+cd /home/chatbot && python3 webChat.py botName=Ansible_Expert title=Ansible_Chabot subtitle=Red_Hat_Demo comment=Latam_Technology_Office questionSentence=Yout_Question? buttonT
 ext=Do_Ask
