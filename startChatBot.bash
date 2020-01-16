625851f3808c9a4381f8b97d07b49a003910f692
RHEL Latam Technology Office <59034224+ltoRhelDemos@users.noreply.github.com>
2020-01-16 08:50:05 -0500

    Delete startChatBot.bash

--- startChatBot.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-/usr/bin/python3 serviceProvider.py &
-
-/usr/bin/python3 webChat.py botName=Ansible_Expert title=Ansible_Chatter subtitle=Latam_Technology_Office comment=maintained_by_Alejandro_Dirgan questionSentence=Your_question? buttonText=Ask&
-
-
