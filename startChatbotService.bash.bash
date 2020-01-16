0ffc2ea5a3ee7eb7a3cd8e78426b701c01c8bfa6
RHEL Latam Technology Office <59034224+ltoRhelDemos@users.noreply.github.com>
2020-01-16 08:51:11 -0500

    Create startChatbotService.bash.bash

---
+++ startChatbotService.bash.bash
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+python3 ./webChat.py botName=Ansible_Expert title=Ansible_Chabot subtitle=Red_Hat_Demo comment=Latam_Technology_Office questionSentence=Yout_Question? buttonT
+ext=Do_Ask
+
